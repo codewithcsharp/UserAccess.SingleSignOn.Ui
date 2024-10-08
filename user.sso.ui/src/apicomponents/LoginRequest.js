428ed2fcc71a132395f4ddb6dc324227df244a43
Ashwini Kumar <ashwiniyadav1702@gmail.com>
2024-10-08 17:16:25 +0530

    added login and register consume file

---
+++ user.sso.ui/src/apicomponents/LoginRequest.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+const [loading, setLoading] = useState(true);
+const [result, setResult] = useState(null);
+const [error, setError] = useState(null);
+
+useEffect(() => {
+  const LoginRequestAsync = async () => {
+	try {
+	  const request = await fetch('https://localhost:44398/api/Users/loginrequest', {
+		method: 'POST',
+		headers: {
+		  'content-type': 'application/json'
+		},
+		body: JSON.stringify({request})
+	  })
+	  if(!request.ok)
+		throw new Error('');
+	  const response = await request.json();
+	  setResult(response);
+	}
+	catch(error) {
+	  setError(error.message);
+	} 
+	finally {
+	  setLoading(false);
+	}
+};
+	LoginRequestAsync();
+}, []);
+
+if(loading) {
+  return (<div>Please wait loading...</div>);
+}
+
+if (error) {
+  return (<div>Something went wrong...</div>);
+}
+  return (<div>{JSON.stringify(result)}</div>)
+
+export default LoginRequestAsync;
---
+++ user.sso.ui/src/apicomponents/RegistrationRequest.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
+const [loading, setLoading] = useState(true);
+const [result, setResult] = useState(null);
+const [error, setError] = useState(null);
+
+useEffect(() => {
+    const RegistrationRequestAsync = async () => {
+        try {
+        const request = await fetch('https://localhost:44398/api/Users/registranstion', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({request})
+        })
+        if(!request.ok) 
+            throw new Error('');
+            const response = await request.json();
+            setResult(response);
+        } catch (error) {
+            setError(error.message);
+        } 
+		finally {
+            setLoading(false);
+        }
+    };
+    RegistrationRequestAsync();
+}, []);
+
+if(loading) {
+	return (<div>Please wait loading...</div>);
+}
+if(error) {
+	return (<div>Something went wrong...</div>);
+}
+return (<div>{JSON.stringify(result)}</div>);
+
+export default RegistrationRequestAsync;
